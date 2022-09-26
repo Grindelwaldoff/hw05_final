7e6c8e76981a63548943a00b90952a68c07f9944
Grindelwaldoff <vsevolodrybnik@gmail.com>
2022-09-26 18:22:36 +0300

    review 6/1

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -1,17 +1,15 @@
-from ctypes import addressof
-from re import template
 from django.test import TestCase, Client
-from django.urls import reverse
 
 
 class TestErrorsUrl(TestCase):
     def setUp(self):
         self.cleint = Client()
 
-    def test_404_and_403_urls(self):
+    def test_404_and_403_and_500_urls(self):
         check_list = [
             ('core/404.html', '/smth/idk/'),
             # ('core/403csrf.html', '???') не знаю какую ссылку
+            # ('core/500.html', '???') тоже самое
         ]
         for tuple in check_list:
             template, address = tuple
